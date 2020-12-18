a56ae199927363b325b885c875d1e66d446de620
ertugrulcetin <ertu.ctn@gmail.com>
2020-12-18 23:06:14 +0300

    added tets examples dir

---
+++ test/examples/README.md
@@ -0,0 +1,1 @@
+This tutorials are Clojure version of https://wiki.jmonkeyengine.org/docs/3.3/tutorials/beginner/beginner.html
---
+++ test/examples/beginner_tutorials/hello_simple_app.clj
@@ -0,0 +1,27 @@
+(ns examples.beginner-tutorials.hello-simple-app
+  "Clojure version of https://wiki.jmonkeyengine.org/docs/3.3/tutorials/beginner/hello_simpleapplication.html"
+  (:require
+   [jme-clj.core :refer :all])
+  (:import
+   (com.jme3.math ColorRGBA)))
+
+
+(defn init []
+  (let [box           (box 1 1 1)
+        geom          (geo "Box" box)
+        mat           (material "Common/MatDefs/Misc/Unshaded.j3md")]
+    (set* mat :color "Color" ColorRGBA/Blue)
+    (set* geom :material mat)
+    (add-to-root geom)))
+
+
+(defsimpleapp app :init init)
+
+
+(comment
+ (start app)
+ (stop app)
+
+ (re-init app init)
+
+ (unbind-app #'app))
