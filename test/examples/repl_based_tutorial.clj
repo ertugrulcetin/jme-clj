c9b57ba2363731ce5297a4274d8c24e994629a5d
ertugrulcetin <ertu.ctn@gmail.com>
2020-12-20 22:02:41 +0300

    repl-based-tutorial update

--- test/examples/repl_based_tutorial.clj
+++ test/examples/repl_based_tutorial.clj
@@ -1,5 +1,5 @@
 ;; A REPL based, jme-clj tutorial
-;; Please visit https://github.com/ertugrulcetin/jme-clj for more info
+;; Please visit https://github.com/ertugrulcetin/jme-clj for more info.
 ;;
 ;; This is a very basic intro that covers jme-clj's basic features and philosophy.
 ;;
@@ -15,49 +15,49 @@
 
  (import '(com.jme3.math ColorRGBA))
 
- ;; Our simple init fn, it creates a cube with Monkey.jpg on the surface.
+ ;; Our simple init fn. It creates a cube with jMonkeyEngine's logo on the surface.
  (defn init []
-   (let [player (geo "jMonkey cube" (box 1 1 1))
-         mat    (unshaded-mat)]
+   (let [cube (geo "jMonkey cube" (box 1 1 1))
+         mat  (unshaded-mat)]
      (set* mat :texture "ColorMap" (load-texture "Interface/Logo/Monkey.jpg"))
-     (set* player :material mat)
-     (add-to-root player)))
+     (set* cube :material mat)
+     (add-to-root cube)))
 
  ;; Let's create simple-update fn with no body for now.
  (defn simple-update [tpf])
 
- ;; We define a `app` var.
+ ;; We define the `app` var.
  (defsimpleapp app
                :init init
                :update simple-update)
 
- ;; Here we go. You'll see jMonkeyEngine's dialog box, after hitting the "Continue" button
+ ;; Here we go. You'll see jMonkeyEngine's dialog box. After hitting the "Continue" button,
  ;; our app will start.
  (start app)
 
- ;; It's time to add some magic, let's rotate the cube. First of all we're going to re-define the init fn
- ;; Can you spot the only change? Yes, we're returning hash map that holds player data.
- ;; When init fn returns a hash map, it's value registered into the app's global mutable state,
- ;; so other fns can access for later use. This applies for the update fn as well.
+ ;; It's time to add some magic. Let's rotate the cube. First of all, we're going to re-define the init fn
+ ;; Can you spot the only change? Yes, we're returning a hash map that holds cube data.
+ ;; When init fn returns a hash map, its value is registered into the app's global mutable state,
+ ;; so other fns can access it for later use. This applies to the update fn as well.
  (defn init []
-   (let [player (geo "jMonkey cube" (box 1 1 1))
-         mat    (unshaded-mat)]
+   (let [cube (geo "jMonkey cube" (box 1 1 1))
+         mat  (unshaded-mat)]
      (set* mat :texture "ColorMap" (load-texture "Interface/Logo/Monkey.jpg"))
-     (set* player :material mat)
-     (add-to-root player)
-     {:player player}))
+     (set* cube :material mat)
+     (add-to-root cube)
+     {:cube cube}))
 
- ;; Let's call re-init (Re-initializes the app) so we can let the app know about the changes.
+ ;; Let's call re-init (Re-initializes the app), so we can let the app know about the changes.
  ;; re-init is like restart but does not call stop and start fns.
  (run app
       (re-init init))
 
- ;; When you focus your cursor to the app, you'll see that the cube is rotating. Update fn (simple-update) does not
- ;; need something like `re-update` fn. init fn is only called once that's why we re-initialize with re-init,
- ;; on the other hand simple-update is constantly called by the app.
+ ;; When you focus your cursor on the app, you'll see that the cube is rotating. Update fn (simple-update) does not
+ ;; need something like `re-update` fn. init fn is only called once. That's why we re-initialize with re-init.
+ ;; On the other hand, the app is constantly calling the simple-update fn.
  (defn simple-update [tpf]
-   (let [{:keys [player]} (get-state)]
-     (rotate player 0 (* 2 tpf) 0)))
+   (let [{:keys [cube]} (get-state)]
+     (rotate cube 0 (* 2 tpf) 0)))
 
  ;; `unbind-app` destroys the app, unbinds the `app` var. Which means it's gone for good. It can be used for
  ;; development purposes, e.g. when you want to create an app with different attributes.
@@ -75,20 +75,41 @@
                :init init
                :update simple-update)
 
- ;; Starting the new app. Now, you won't see jMonkeyEngine's dialog because we set :show-settings? to false.
- ;; Since we set :pause-on-lost-focus? to false, we don't have to focus the cursor, cube is rotating.
+ ;; We can start the new app. Now, you won't see jMonkeyEngine's dialog because we set :show-settings? to false.
+ ;; Since we set :pause-on-lost-focus? to false, we don't have to focus the cursor, the cube is rotating.
  ;; FPS limit set to 60, and we have a new title `My JME Game`. Finally, resolution is 800x600.
  (start app)
 
  ;; We can change the cube's position by setting its local translation.
  ;; Please note that every state change related code should we wrapped with run macro!
  (run app
-      (let [{:keys [player]} (get-state)]
-        (set* player :local-translation (add (get* player :local-translation) 1 1 1))))
+      (let [{:keys [cube]} (get-state)]
+        (set* cube :local-translation (add (get* cube :local-translation) 1 1 1))))
 
- ;; By default, there is Fly Camera attached to the app that you can control with W,A,S,D keys. Let's increase it's
- ;; move speed. Now, you fly faster :)
+ ;; By default, there is a Fly Camera attached to the app that you can control with W, A, S and D keys.
+ ;; Let's increase its movement speed. Now, you fly faster :)
  (run app
       (set* (fly-cam) :move-speed 15))
 
+ ;; It would be cool to add a new cube. Let's go for it.
+ (run app
+      (let [cube (geo "jMonkey cube" (box 1 1 1))
+            mat  (unshaded-mat)]
+        (set* mat :texture "ColorMap" (load-texture "Interface/Logo/Monkey.jpg"))
+        (setc cube
+              :material mat
+              :local-translation [-3 0 0])
+        (add-to-root cube)
+        (set-state :cube2 cube)))
+
+ ;; We added the new cube, but it's not rotating. We need to update the simple-update fn.
+ (defn simple-update [tpf]
+   (let [{:keys [cube cube2]} (get-state)]
+     (rotate cube 0 (* 2 tpf) 0)
+     (rotate cube2 0 (* 2 tpf) 0)))
+
+ ;; I hope you liked the tutorial! One of the best things about writing a game in Clojure is that
+ ;; you can modify it in a REPL while it's running. It's cool to have hot reloading for 3D game development.
+
+ ;; If you have any questions, please reach me out through https://github.com/ertugrulcetin/jme-clj/issues
  )
